0fbb388d1cd50f0a7f1d1b2f8026eb0e19c73b5e
tkoooo <100322984+tkoooo@users.noreply.github.com>
2024-02-14 21:30:08 -0600

    fixed bugs during SFR2 testing

--- milestone5_datalogger.py
+++ milestone5_datalogger.py
@@ -29,6 +29,17 @@
 
 """
 
+"""
+# SFR2 TESTING:
+# 1 - 101 - L
+# 2 - 102 - G
+# 3 - 103 - H
+# 4 - 104 - I
+# 5 - 105 - J
+# 6 - 106 - K
+# '@101, 102, 103, 104, 105, 106'
+"""
+
 
 import numpy as np # For keysight_ktdaq970 arrays
 import pyvisa as visa
@@ -39,7 +50,6 @@ import time
 def main():
     Channel_Dict = {105: 'TC1', 106: 'TC2', 107: 'TC3', 108: 'TC4', 109: 'PV Inlet', 110: 'TR Outlet', 111: 'PV Outlet', 112: 'TR Inlet', 113: 'TR Pump Outlet P', 
                     114: 'TR Outlet P', 120: 'Outside Temp', 201: 'TC5', 202: 'TC6', 203: 'TC7', 204: 'TC8'}
-
 
     rm = visa.ResourceManager() # VISA Resource Manager keeps track of instruments and creates sessions to them as requested
     addr = 'USB0::0x0957::0x2007::MY49016664::0::INSTR' # Grabbed from Keysight Connection Expert
@@ -54,18 +64,18 @@ def main():
         logger = rm.open_resource(addr) # Open session for the datalogger with resource manager
         print("\n Manufacturer/Model/Serial/Firmware level: ", logger.query('*IDN?')) # Reads out manufacturer, model, serial number, firmware level and options in an ASCII response data element
 
-        #logger.timeout =  10000
+        logger.timeout =  10000
         #logger.write('*RST') # Resets the instrument - factory reset every time ### TOGGLED OFF
 
         # You only need to set the time and date once if you don't reset every time
-        #logger.write("SYST:DATE 2023,12,14") #<yyyy>,<mm>,<dd>  ### TOGGLED OFF
-        #logger.write("SYST:TIME 16,44,00.000") #SYSTem:TIME <hh>,<mm>,<ss.sss> ### TOGGLED OFF
+        #logger.write("SYST:DATE 2023,2,14") #<yyyy>,<mm>,<dd>  ### TOGGLED OFF
+        #logger.write("SYST:TIME 16,16,00.000") #SYSTem:TIME <hh>,<mm>,<ss.sss> ### TOGGLED OFF
 
         #print("time testing\n") 
         #print(logger.write("FORM:READ:TIME:TYPE?"))
 
 
-        """
+    
         # RESISTANCE READINGS UPDATED 11/27/23
         # WE PROBABLY WON'T NEED THIS CODE 
         #SENSe - This command disables or enables autoranging for 2-wire (RESistance) or 4-wire (FRESistance) resistance measurements on the specified channels.
@@ -77,22 +87,22 @@ def main():
         #   does not place the instrument in the "wait-for-trigger" state. Use the INITiate or READ? command in conjunction with CONFigure to place the instrument 
         #   in the "wait-for-trigger" state.
         #logger.write(':CONFigure:RESistance (%s)' % ('@102, 103'))
-        """
+
 
 
         # TEMPERATURE READINGS
         # SENSe - This checks that thermocouple is properly connected 
-        logger.write(':SENSe:TEMPerature:TRANsducer:TCouple:CHECk %d,(%s)' % (1, '@105, 106, 107, 108, 201, 202, 203, 204'))
+        logger.write(':SENSe:TEMPerature:TRANsducer:TCouple:CHECk %d,(%s)' % (1, '@101, 102, 103, 104, 105, 106'))
         # SENSe - This selects the thermocouple type to use
-        logger.write(':SENSe:TEMPerature:TRANsducer:TCouple:TYPE %s,(%s)' % ('K', '@105, 106, 107, 108, 201, 202, 203, 204'))
+        logger.write(':SENSe:TEMPerature:TRANsducer:TCouple:TYPE %s,(%s)' % ('K', '@101, 102, 103, 104, 105, 106'))
 
         # CONFigure - These commands configure the channels for temperature measurements but do not initiate the scan.
-        logger.write(':CONFigure:TEMPerature %s,%s,(%s)' % ('TCouple', 'K', '@105, 106, 107, 108, 201, 202, 203, 204')) ### TOGGLED OFF
+        logger.write(':CONFigure:TEMPerature %s,%s,(%s)' % ('TCouple', 'K', '@101, 102, 103, 104, 105, 106')) ### TOGGLED OFF
 
         # ROUT - This command selects the channels to be included in the scan list. This command is used in conjunction with the CONFigure commands to set up an 
         #    automated scan. The specified channels supersede any channels previously defined to be part of the scan list. To start the scan, use the INITiate or 
         #    READ? command. 
-        logger.write(':ROUTe:SCAN (%s)' % ('@105, 106, 107, 108, 201, 202, 203, 204')) # this is where you put multiple
+        logger.write(':ROUTe:SCAN (%s)' % ('@101, 102, 103, 104, 105, 106')) # this is where you put multiple
 
         # READ? - This command changes the instrument's triggering system from the "idle" state to the "wait-for-trigger" state. Scanning will begin when the specified 
         #   trigger conditions are satisfied following the receipt of the READ? command. Readings are then sent immediately to reading memory and the instrument's 
@@ -116,14 +126,16 @@ def main():
         logger.write(':FORMat:READing:TIME:TYPE %s' % ('ABS'))
         logger.write(':FORMat:READing:ALARm %d' % (1))
 
-        for n in range(3):
+        
+        # LOOPING
+        for n in range(15): #time delay of 2 seconds means 2*30 = 60 so n=30 for 1 minute, n=150 for 5 minutes, n=90 for 3 minutes
             # Display time of this measurement and also write to file
             time_values = logger.query_ascii_values(':SYSTem:TIME?')
             hh = int(time_values[0])
             mm = int(time_values[1])
             sss = time_values[2]
-            print("\n Current time: ", hh, ":", mm)
-            time_string = "Current time: " + str(hh) + ":" + str(mm) + "\n"
+            print("\n\n Current time: ", hh, ":", mm)
+            time_string = "\nCurrent time: " + str(hh) + ":" + str(mm) + "\n"
             file_object.write(time_string)
 
             # FETCh? - This command transfers readings stored in non-volatile memory to the instrument's output buffer, where you can read them into your computer. The 
@@ -134,8 +146,19 @@ def main():
             # Here we use regex to find expressions starting with a + and also contains a second + until we reach the next group starting with a +
             # Format of 2 readings: example = "+1.46376840E+02 OHM,102,0,+9.90000000E+37 OHM,103,0"
             # Format of 1 reading: "+reading,channel,time"
-            pattern = re.compile(r'[+].*?([+][0-9A-Z, ]+)') #regex
-            assignments = [Channel_Dict[105], Channel_Dict[106], Channel_Dict[107], Channel_Dict[108], Channel_Dict[201], Channel_Dict[202], Channel_Dict[203], Channel_Dict[204]]
+            #pattern = re.compile(r'[+].*?([+][0-9A-Z, ]+)') #old regex - wasn't correct
+            pattern = re.compile(r'([+-]?\d*\.?\d+E[+-]?\d+)\s*([A-Za-z,0-9]+)')  # new regex pattern
+                # [+-]? matches optional sign
+                # \d* matches 0-any digits
+                # \.? is optional decimal
+                # \d+ is 1-any digits
+                # E is exponential for sci notation
+                # [+-]?\d+ is exponent part with optional sign and 1-any digits
+                # \s* is 0-any whitespaces
+                # ([A-Za-z,0-9]+) is 1-any characters of letters or numbers
+
+            #assignments = [Channel_Dict[105], Channel_Dict[106], Channel_Dict[107], Channel_Dict[108], Channel_Dict[109], Channel_Dict[110]]
+            assignments = ['TC1 - L', 'TC2 - G', 'TC3 - H', 'TC4 - I', 'TC5 - J', 'TC6 - K']
             count = 0
             for m in re.finditer(pattern, readings1): # need to iterate because we need to read multiple channels
                 res = m.group(0)
@@ -144,12 +167,12 @@ def main():
                 count +=1
                 file_object.write(out) # write out to file
                 file_object.write("\n")
-            time.sleep(1)
+            time.sleep(2)
 
         
         print("\n Finished requested reading")
 
-
+        """
         # ALARM
         logger.write(':OUTPut:ALARm1:SOURce (%s)' % ('@105, 106, 107, 108, 201, 202, 203, 204')) 
         #assigns one of four alarm numbers to report any alarm conditions on the specified multiplexer or digital channels.
@@ -163,6 +186,7 @@ def main():
         temp_values = logger.query_ascii_values(':STATus:ALARm:EVENt?')
         #This command queries the event register for the Alarm Register group.
         register = int(temp_values[0])
+        """
 
 
     except Exception as e:
